5489d52d762bb8f4d2862390042d6792ab50ef15
pedrodalvy <pedrodalvy@outlook.com>
2020-05-05 10:08:21 -0400

    Criada a view para mostrar mensagens de erro

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,28 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
     <div class="container">
+        @include('partials._alert')
+
         <form action="{{ route('admin.users.store') }}" method="POST">
             @csrf
 
             <div class="form-group">
                 <label for="name">Nome</label>
-                <input type="name" class="form-control" id="name">
+                <input type="name" class="form-control" id="name" name="name">
             </div>
 
             <div class="form-group">
                 <label for="email">E-mail</label>
-                <input type="email" class="form-control" id="email">
+                <input type="email" class="form-control" id="email" name="email">
             </div>
 
             <button type="submit" class="btn btn-primary mb-2 btn-block">Inserir</button>
