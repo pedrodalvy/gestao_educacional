ecfb90b3b0f94636a4e02d93bb09294029470c62
pedrodalvy <pedrodalvy@outlook.com>
2020-05-06 19:57:47 -0400

    Update _toastr.blade.php

--- resources/views/partials/_toastr.blade.php
+++ resources/views/partials/_toastr.blade.php
@@ -9,6 +9,24 @@
     @php $message = Session::get('message'); @endphp
 
     <script>
+        toastr.options = {
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "500",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+
         const type = "{{ $message['type-message'] }}";
 
         switch (type) {
