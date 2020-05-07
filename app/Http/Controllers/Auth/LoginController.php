8af05ed7d6196cf862add65ce366de6624f3e385
pedrodalvy <pedrodalvy@outlook.com>
2020-05-06 20:59:11 -0400

    Adicionada opção para fazer login com matricula

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,44 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function credentials(Request $request)
+    {
+        $data = $request->only($this->username(), 'password');
+        $usernameKey = $this->usernameKey();
+
+        $data[$usernameKey] = $data[$this->username()];
+        unset($data[$this->username()]);
+
+        return $data;
+    }
+
+    /**
+     * Check if the login is with email or enrolment
+     *
+     * @return string
+     */
+    protected function usernameKey()
+    {
+        $email = \Request::get($this->username());
+
+        $validator = \Validator::make([
+            'email' => $email
+        ], [
+            'email' => 'email'
+        ]);
+
+        return $validator->fails() ? 'enrolment' : 'email';
+    }
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'username';
     }
 }
