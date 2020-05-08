837ad37b51ed04d22f0ed5a6b17d9da52a6049fd
pedrodalvy <pedrodalvy@outlook.com>
2020-05-07 21:23:23 -0400

    Correção na validação de atualização de cadastro de usuário

--- app/Http/Requests/Admin/UpdateUsersRequest.php
+++ app/Http/Requests/Admin/UpdateUsersRequest.php
@@ -26,7 +26,7 @@ class UpdateUsersRequest extends FormRequest
         return [
             'name' => 'required|max:255',
             'email' => 'required|max:255|email',
-            'type' => 'required|in:1,2,3',
+            'type' => 'in:1,2,3',
         ];
     }
 }
