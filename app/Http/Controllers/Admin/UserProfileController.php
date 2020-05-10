17d4116f395a3bbd6a3c178e3b46ea21e3841c3f
pedrodalvy <pedrodalvy@outlook.com>
2020-05-09 21:23:10 -0400

    Criada validação para o perfil do usuário

--- app/Http/Controllers/Admin/UserProfileController.php
+++ app/Http/Controllers/Admin/UserProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\UserProfileRequest;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -16,11 +17,28 @@ class UserProfileController extends Controller
 
     public function update(User $user, Request $request)
     {
+        $this->validateProfile($request);
+
         $user->profile->address
             ? $user->profile->update($request->all())
             : $user->profile()->create($request->all());
 
         toastr('success', 'Perfil atualizado com sucesso');
         return back();
+    }
+
+    protected function validateProfile(Request $request)
+    {
+        $rules = [
+            'address' => 'required|string',
+            'cep' => 'required|string|max:8',
+            'number' => 'required|numeric',
+            'complement' => 'nullable|string',
+            'neighborhood' => 'required|string',
+            'city' => 'required|string',
+            'state' => 'required|string|max:2',
+        ];
+
+        return $request->validate($rules);
     }
 }
