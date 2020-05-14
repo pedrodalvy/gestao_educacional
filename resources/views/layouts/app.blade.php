7c40307d24cdffaee61fa490357a94e8a07fbe50
pedrodalvy <pedrodalvy@outlook.com>
2020-05-13 21:20:24 -0400

    Correção de erro ortográfico

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Gestão Educional</title>
+    <title>Gestão Educacional</title>
 
 
     <!-- Fonts -->
