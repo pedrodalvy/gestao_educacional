97c141a827e1a0c6c6bdaebcc1a4b279ea8846c0
pedrodalvy <pedrodalvy@outlook.com>
2020-05-13 21:18:01 -0400

    Configurado o select2 para busca de alunos

--- resources/assets/admin/js/components/class_information/ClassStudent.vue
+++ resources/assets/admin/js/components/class_information/ClassStudent.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <div class="form-group">
-            <label for="students" class="control-label">Selecionar estudante</label>
-            <select name="students" id="students" class="form-control"></select>
+            <label class="control-label" for="students">Selecionar estudante</label>
+            <select class="form-control" id="students" name="students"></select>
         </div>
 
         <table class="table table-bordered">
@@ -13,17 +13,19 @@
             </tr>
             </thead>
             <tbody>
-                <tr v-for="student in students">
-                    <td>Excluir</td>
-                    <td>{{ student.user.name }}</td>
-                </tr>
+            <tr v-for="student in students">
+                <td>Excluir</td>
+                <td>{{ student.user.name }}</td>
+            </tr>
             </tbody>
         </table>
     </div>
 </template>
 
 <script>
+    import ADMIN_CONFIG from '../../services/adminConfig';
     import store from '../../store/store';
+    import 'select2';
 
     export default {
         props: [
@@ -35,7 +37,30 @@
             }
         },
         mounted() {
-            store.dispatch('classStudent/query', this.classInformation)
+            // Preenche a tabela com a lista de estudantes
+            store.dispatch('classStudent/query', this.classInformation);
+
+            // Cria o select2 para pesquisa de estudantes
+            $("select[name=students]").select2({
+                ajax: {
+                    url: `${ADMIN_CONFIG.API_URL}/students`,
+                    dataType: 'json',
+                    delay: 250,
+                    data(params) {
+                        return {
+                            q: params.term
+                        }
+                    },
+                    processResults(data) {
+                        return {
+                            results: data.map(student => {
+                                return {id: student.id, text: student.user.name}
+                            })
+                        }
+                    }
+                },
+                minimumInputLength: 1,
+            });
         }
     }
 </script>
