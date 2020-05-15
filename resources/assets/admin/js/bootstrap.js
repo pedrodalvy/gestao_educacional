8f4aefaa550f81c05256bcb4a8a99baa6f946f66
pedrodalvy <pedrodalvy@outlook.com>
2020-05-14 22:12:57 -0400

    Update bootstrap.js

--- resources/assets/admin/js/bootstrap.js
+++ resources/assets/admin/js/bootstrap.js
@@ -1,6 +1,8 @@
 try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
+    window.Toastr = require('toastr');
+    require('toastr/build/toastr.min.css');
 
     require('bootstrap');
 } catch (e) {}
