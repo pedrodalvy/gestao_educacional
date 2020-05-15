84635dff9005e7b08a63aa274e56257dfca7bd8c
pedrodalvy <pedrodalvy@outlook.com>
2020-05-14 22:13:43 -0400

    Refatoração da view com o alert das telas do admin

--- resources/views/partials/_toastr.blade.php
+++ resources/views/partials/_toastr.blade.php
@@ -1,52 +1,16 @@
 @if(Session::has('message'))
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
-            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    <link rel="stylesheet" type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-
     @php $message = Session::get('message'); @endphp
 
     <script>
-        toastr.options = {
-            "closeButton": false,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": true,
-            "positionClass": "toast-top-right",
-            "preventDuplicates": false,
-            "onclick": null,
-            "showDuration": "500",
-            "hideDuration": "1000",
-            "timeOut": "5000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-        }
-
-        const type = "{{ $message['type-message'] }}";
-
-        switch (type) {
-            case 'info':
-                toastr.info("{{ $message['message'] }}");
-                break;
-
-            case 'warning':
-                toastr.warning("{{ $message['message'] }}");
-                break;
-
-            case 'success':
-                toastr.success("{{ $message['message'] }}");
-                break;
-
-            case 'error':
-                toastr.error("{{ $message['message'] }}");
-                break;
-        }
-
-
+        window.onload = function () {
+            Toastr.options = {
+                "progressBar": true,
+                "positionClass": "toast-top-right",
+                "showDuration": "500",
+            }
+            const type = "{{ $message['type-message'] }}";
+            Toastr[type]("{{ $message['message'] }}");
+        };
     </script>
 @endif
