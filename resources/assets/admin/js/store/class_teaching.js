fe9e99c9324828efc74c4d979280b0cdaf3a8849
pedrodalvy <pedrodalvy@outlook.com>
2020-05-16 11:16:35 -0400

    Criado store para administração de disciplinas no vue

---
+++ resources/assets/admin/js/store/class_teaching.js
@@ -0,0 +1,53 @@
+import {ClassTeaching} from "../services/resources";
+import Vue from 'vue';
+import ADMIN_CONFIG from '../services/adminConfig';
+
+const state = {
+    teachings: []
+};
+
+const mutations = {
+    add(state, teaching) {
+        state.teachings.push(teaching);
+    },
+    set(state, teachings) {
+        state.teachings = teachings;
+    },
+    destroy(state, teachingId) {
+        let index = state.teachings.findIndex(item => {
+            return item.id === teachingId;
+        });
+        if (index !== -1) {
+            state.teachings.splice(index, 1);
+        }
+    }
+};
+
+const actions = {
+    query(context, classInformationId) {
+        return Vue.http.get(`${ADMIN_CONFIG.ADMIN_URL}/classinformation/${classInformationId}/teachings`)
+            .then(response => {
+                context.commit('set', response.data);
+            })
+    },
+    store(context, {teacherId, subjectId, classInformationId}) {
+        return ClassTeaching.save({classinformation: classInformationId},
+            {teacher_id: teacherId, subject_id: subjectId})
+            .then(response => {
+                context.commit('add', response.data)
+            })
+    },
+    destroy(context, {teachingId, classInformationId}) {
+        return ClassTeaching.delete({classinformation: classInformationId, teaching: teachingId})
+            .then(() => {
+                context.commit('destroy', teachingId)
+            })
+    },
+};
+
+const module = {
+    namespaced: true,
+    state, mutations, actions
+}
+
+export default module;
