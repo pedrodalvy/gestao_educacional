864f5ab76e44408f50339d82273916ce9dafb07b
pedrodalvy <pedrodalvy@outlook.com>
2020-05-16 11:17:26 -0400

    Alterada a seeder de turmas para adição de professores/disciplinas em turmas

--- database/seeds/ClassInformationSeeder.php
+++ database/seeds/ClassInformationSeeder.php
@@ -2,6 +2,8 @@
 
 use App\Models\ClassInformation;
 use App\Models\Student;
+use App\Models\Subject;
+use App\Models\Teacher;
 use Illuminate\Database\Seeder;
 
 class ClassInformationSeeder extends Seeder
@@ -14,12 +16,25 @@ class ClassInformationSeeder extends Seeder
     public function run()
     {
         $students = Student::all();
+        $teachers = Teacher::all();
+        $subjects = Subject::all();
 
         factory(ClassInformation::class, 50)
             ->create()
-            ->each(function (ClassInformation $model) use ($students) {
+            ->each(function (ClassInformation $model) use ($students, $teachers, $subjects) {
                 $studentsCollection = $students->random(10);
                 $model->students()->attach($studentsCollection->pluck('id'));
+
+                $teaching = rand(3, 6);
+
+                $teachersCollection = $teachers->random($teaching);
+                $subjectsCollection = $subjects->random($teaching);
+                foreach (range(1, $teaching) as $value) {
+                    $model->teachings()->create([
+                        'subject_id' => $subjectsCollection->get($value - 1)->id,
+                        'teacher_id' => $teachersCollection->get($value - 1)->id,
+                    ]);
+                }
             });
     }
 }
