abc12633d9b4e1974627269d7b9f6bd4bd9f465c
pedrodalvy <pedrodalvy@outlook.com>
2020-05-17 18:56:11 -0400

    Criada rota para login no vue

--- resources/assets/spa/js/router.map.js
+++ resources/assets/spa/js/router.map.js
@@ -1,8 +1,16 @@
-
 export default [
     {
         name: 'class_informations.list',
         path: '/classes',
         components: require('./components/teacher/TeacherClassInformationList')
+    },
+    {
+        name: 'login',
+        path: '/login',
+        components: require('./components/Login')
+    },
+    {
+        path: '*',
+        redirect: '/login'
     }
 ]
