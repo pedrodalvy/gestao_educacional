21d34071e030aab9b8dbae7f7121a6087a43bdaa
pedrodalvy <pedrodalvy@outlook.com>
2020-05-17 17:39:16 -0400

    Criado menu de navegação

---
+++ resources/assets/spa/js/components/teacher/TeacherMenu.vue
@@ -0,0 +1,15 @@
+<template src="../templates/menu.htm"></template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                menus: [
+                    {name: "Minhas Turmas", routeName: 'class_informations.list'}
+                ],
+                brandRouteName: 'class_informations.list'
+            }
+        }
+    };
+</script>
