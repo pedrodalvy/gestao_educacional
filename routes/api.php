3eb3ec1f72915dde750f65adf9e8e80f4a94ef3e
pedrodalvy <pedrodalvy@outlook.com>
2020-05-17 10:46:34 -0400

    Criação de grupo de rotas protegido por autenticação

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
 
 Route::group([
     'as' => 'api',
     'namespace' => 'Api\\'
 ], function () {
     Route::post('/access_token', 'AuthController@accessToken');
-    Route::post('/logout', 'AuthController@logout');
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('/logout', 'AuthController@logout');
+    });
 });
